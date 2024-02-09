fb9b8291c2f85d5ffd6fa202b2096918b6babdfe
Sebastian Lachut <seb.lachut@gmail.com>
2024-02-09 19:41:26 +0100

    Created a TicTacToeRunner class to validate the input data and moved inilialization to Main

--- src/main/java/com/tictactoe/Main.java
+++ src/main/java/com/tictactoe/Main.java
@@ -4,21 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Board b = new Board();
-        b.displayBoard();
-        while (!b.gameFinished()) {
-            Scanner s = new Scanner(System.in);
-            System.out.println("Enter row 1-3");
-            int row = s.nextInt() - 1;
-            s.nextLine();
-            System.out.println("Enter col 1-3");
-            int col = s.nextInt() - 1;
-            s.nextLine();
-            b.getPlayerFigure(row, col);
-            System.out.println("Player made a move");
-            b.getComFigure();
-            System.out.println("Com made a move");
-            b.displayBoard();
-        }
+        TicTacToeRunner runner = new TicTacToeRunner();
+        runner.play();
+
     }
 }
