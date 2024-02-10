df5e60c62993c1bd2defb9c04d99318ab98a0269
Sebastian Lachut <seb.lachut@gmail.com>
2024-02-10 12:51:02 +0100

    Created validation method for input data

--- src/main/java/com/tictactoe/TicTacToeRunner.java
+++ src/main/java/com/tictactoe/TicTacToeRunner.java
@@ -7,19 +7,41 @@ public class TicTacToeRunner {
     public void play() {
         Board b = new Board();
         b.displayBoard();
+        int row;
+        int col;
         while (!b.gameFinished()) {
             Scanner s = new Scanner(System.in);
-            System.out.println("Enter row 1-3");
-            int row = s.nextInt() - 1;
-            s.nextLine();
+
+            System.out.println("Enter row (1-3)");
+            row = validateValue(s);
+
             System.out.println("Enter col 1-3");
-            int col = s.nextInt() - 1;
-            s.nextLine();
+            col = validateValue(s);
+
             b.getPlayerFigure(row, col);
+
             System.out.println("Player made a move");
             b.getComFigure();
             System.out.println("Com made a move");
             b.displayBoard();
         }
+    }
+
+    private int validateValue(Scanner s) {
+        int value;
+        while (true) {
+            if (s.hasNextInt()) {
+                value = s.nextInt() - 1;
+                if (value < 0 || value >= 3) {
+                    System.out.println("Enter proper number (1-3)");
+                } else {
+                    break;
+                }
+            } else {
+                System.out.println("Type a number (1-3)");
+                s.next();
+            }
+        }
+        return value;
     }
 }
